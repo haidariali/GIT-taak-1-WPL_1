45fe123339eb84561cdc5382bc6102a567fe31c4
haidariali <ali.haidari@student.pxl.be>
2024-10-09 20:23:40 +0200

    MOVIE-01: Venster - De achtergrondkleur van het scherm is 'PXL Groen' (#58A518)

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace film_app_project_code_ali
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
